9d51059c087e590b7a9e93c98e6d8a64a87bbd85
PMartn <71846491+PMartn@users.noreply.github.com>
2023-09-20 01:04:10 -0300

    Added user search result template

--- screens/Main App/Search/SearchUser.tsx
+++ screens/Main App/Search/SearchUser.tsx
@@ -1,13 +1,38 @@
 import React from 'react';
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Image, ScrollView } from "react-native";
+
+const UserProfile = () => {
+  return (
+    <View style={styles.userProfileContainer}>
+        <Image
+          source={{
+            uri:
+              "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png",
+          }}
+          style={styles.profileImage}
+        />
+        <View>
+          <View style={styles.namesContainer}>
+            <Text style={styles.displayname}>DisplayName </Text>
+            <Text style={styles.username}>@Username </Text>
+          </View>
+          <Text>Bio </Text>
+        </View>
+      </View>
+  )
+}
+
 
 function SearchUser() {
   return (
-    <View
-    style={styles.container}
-    >
-      <Text>User search</Text>
-    </View>
+    <ScrollView contentContainerStyle={styles.container}>
+      <UserProfile/>
+      <UserProfile/>
+      <UserProfile/>
+      <UserProfile/>
+      <UserProfile/>
+      <UserProfile/>
+    </ScrollView>
   );
 }
 
@@ -16,8 +41,31 @@ export default SearchUser;
 
 const styles = StyleSheet.create({
     container: {
-      flex: 1,
-      justifyContent: "center",
       alignItems: "center",
+      paddingVertical: 5
     },
+    userProfileContainer: {
+      width: "95%",
+      backgroundColor: "#ccc",
+      padding: 15,
+      borderRadius: 20,
+      marginVertical: 5,
+      flexDirection: "row",
+    },
+    profileImage: {
+      width: 70,
+      height: 70,
+      borderRadius: 75,
+      marginRight: 15
+    },
+    displayname: {
+      fontSize: 15,
+      fontWeight: "bold",
+    },
+    username: {
+      fontSize: 15,
+    },
+    namesContainer:{
+      flexDirection:"row"
+    }
 });
