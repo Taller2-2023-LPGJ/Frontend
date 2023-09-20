1a5a392965d60b3cd650bacb33734f5e2f5ef88f
PMartn <71846491+PMartn@users.noreply.github.com>
2023-09-20 01:46:38 -0300

    Profiles clickable

--- screens/Main App/Search/SearchUser.tsx
+++ screens/Main App/Search/SearchUser.tsx
@@ -1,31 +1,58 @@
-import React from 'react';
-import { StyleSheet, Text, View, Image, ScrollView } from "react-native";
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Image, ScrollView, TouchableOpacity, TouchableWithoutFeedback } from "react-native";
 
 const UserProfile = () => {
+  const handlePress = () => {
+    console.log("Opened profile")
+  };
+
+
   return (
-    <View style={styles.userProfileContainer}>
-        <Image
-          source={{
-            uri:
-              "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png",
-          }}
-          style={styles.profileImage}
-        />
-        <View>
-          <View style={styles.namesContainer}>
-            <Text style={styles.displayname}>DisplayName </Text>
-            <Text style={styles.username}>@Username </Text>
-          </View>
-          <Text>Bio </Text>
+    
+    <TouchableWithoutFeedback  onPress={handlePress} style={styles.buttonContainer}>
+      <View style={styles.userProfileContainer}>
+      <Image
+        source={{
+          uri:
+            "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png",
+        }}
+        style={styles.profileImage}
+      />
+      <View>
+        <View style={styles.namesContainer}>
+          <Text style={styles.displayname}>DisplayName </Text>
+          <Text style={styles.username}>@Username </Text>
         </View>
+        <Text>Bio </Text>
       </View>
+      </View>
+    </TouchableWithoutFeedback >
+    
   )
 }
 
 
 function SearchUser() {
+
+  const [isPressEnabled, setIsPressEnabled] = useState(true);
+
+
+  const handleScrollBegin = () => {
+    // Disable pressing when scrolling begins
+    setIsPressEnabled(false);
+  };
+
+  const handleScrollEnd = () => {
+    // Enable pressing when scrolling ends
+    setIsPressEnabled(true);
+  };
+
+
   return (
-    <ScrollView contentContainerStyle={styles.container}>
+    <ScrollView contentContainerStyle={styles.container}
+      onScrollBeginDrag={handleScrollBegin}
+      onScrollEndDrag={handleScrollEnd}
+    >
       <UserProfile/>
       <UserProfile/>
       <UserProfile/>
@@ -67,5 +94,9 @@ const styles = StyleSheet.create({
     },
     namesContainer:{
       flexDirection:"row"
+    },
+    buttonContainer:{
+      width: "100%",
+      alignItems: "center",
     }
 });
