74b20761f0beabe81110fbd6040b91b7c0389502
Lucas <lgrati@fi.uba.ar>
2023-09-24 14:34:15 -0300

    fixed navigation error in location->tabNavigator

--- screens/Initial Screens/ChooseLocation.tsx
+++ screens/Initial Screens/ChooseLocation.tsx
@@ -24,7 +24,7 @@ const ChooseLocation = ({ navigation }: Props) => {
       // axios post setLocation...
       console.log(`set user location as ${location}`);
     }
-    setLoggedIn!();
+    await setLoggedIn!();
     navigation.navigate("TabNavigator");
   };
 
